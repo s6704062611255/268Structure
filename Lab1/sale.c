d86d52c351d8be8fb0e69f34c3d2d503dd7f8144
s6704062611255 <s6704062611255@email.kmutnb.ac.th>
2025-12-04 11:56:01 +0700

    add vat 7%

--- Lab1/sale.c
+++ Lab1/sale.c
@@ -1,13 +1,17 @@
 #include<stdio.h>
 int main(){
     float price1,price2,price3;
+    float vat = 0.07;
     scanf("%f",&price1);
-    float total_price1 = price1 - ((price1*0.05));
-    printf("price1 = %.2f\n",total_price1);
+    float total_price1 = (price1 - ((price1*0.05)));
+    float pay1 = total_price1 + total_price1 * vat;
+    printf("price1 = %.2f\n", pay1);
     scanf("%f",&price2);
     float total_price2 = ((price1 + price2) - (((price1 + price2) * 0.15)));
-    printf("price2 = %.2f\n", total_price2);
+    float pay2 = total_price2 + total_price2*vat;
+    printf("price2 = %.2f\n", pay2);
     scanf("%f",&price3);
     float total_price3 = ((price1 + price2 + price3) - (((price1 + price2 + price3) * 0.30)));
-    printf("price3 = %.2f\n", total_price3);
+    float pay3 = total_price3 + total_price3*vat;
+    printf("price3 = %.2f\n", pay3);
 }
